f53cc488b53fc9700f98d3d4aa93ce1218266f07
SammuDev <samucas878@gmail.com>
2023-02-19 10:36:37 -0300

    feat: Criando o controlador de serviço

--- backend/controllers/serviceController.ts
+++ backend/controllers/serviceController.ts
@@ -1,5 +1,19 @@
 const { Service: serviceModel } = require('../models/services');
 
-const serviceController = {};
+const serviceController = {
+  create: async (req, res) => {
+    try {
+      const service = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image,
+      }
+      return await res.status(200).send('TEXTO');
+    } catch (error) {
+      console.log(`ERROR: ${error}`);
+    }
+  }
+};
 
 module.exports = serviceController;
