f01cc766b8a0c39bd40033719b7f843dbf6231d5
SammuDev <samucas878@gmail.com>
2023-02-21 09:21:39 -0300

    feat: Criando resposta JSON para POST

--- backend/controllers/partyController.ts
+++ backend/controllers/partyController.ts
@@ -1,4 +1,4 @@
-const PartyModel = require('../controllers/../controllers/../models/Party.model');
+const PartyModel = require('../models/Party.model');
 
 const checkPartyBudget = (budget, services): boolean => {
   const priceSum = services.reduce((sum, service) => sum + service , 0);
@@ -17,9 +17,11 @@ const partyController = {
         image: req.body.image,
         services: req.body.services
       };
+      const response = await PartyModel.create(partySchema);
       if (partySchema.services && checkPartyBudget(partySchema.budget, partySchema.services)) {
         return res.status(406).json({msg: 'Seu orçamento é insufuciente!'});
-      }
+      };
+      res.status.json({response, msg: 'Evento festivo gerado!'});
     }
     catch (error) {
       return console.log(`ERRO: `, error);
